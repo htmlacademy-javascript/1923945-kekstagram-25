39fe1f1574987ad4b92a276a09db2c736f721f63
Keks <keks@htmlacademy.ru>
2022-03-13 17:07:34 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,11 @@
+import {createCardDescription} from './data.js';
+import {getMaxLengthString} from './utils.js';
+import './gallery.js';
+
+const PHOTO_DESCRIPTION_ARRAY_OBJECTS_LENGTH = 25;
+
+const MAX_LENGTH_STRING = 10;
+
+createCardDescription(PHOTO_DESCRIPTION_ARRAY_OBJECTS_LENGTH);
+
+getMaxLengthString('string', MAX_LENGTH_STRING);
---
+++ 5/js/utils.js
@@ -0,0 +1,24 @@
+const getRandomNumber = (min, max) => {
+  if (min >= 0 && max > min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+};
+
+const getMaxLengthString = (verifiableString, maxLength) => verifiableString.length <= maxLength;
+
+const getShuffleArray = (arr) => {
+  let j, temp;
+  for(let i = arr.length - 1; i > 0; i--){
+    j = Math.floor(Math.random()*(i + 1));
+    temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+export {getRandomNumber, getMaxLengthString, getShuffleArray, getRandomArrayElement};
