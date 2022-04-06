6d47595f0157533d60ec208eb380ed7fd90a284c
Alexandr <santey@bk.ru>
2022-04-06 19:03:13 +0700

    Добавлен фильтр-сортировка

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,20 @@
 import {getPictureData} from './full-size-image.js';
+import {getShuffleArray} from './utils.js';
+
+const FILTER_RANDOM_MAX_ITEMS = 10;
 
 const usersGalleryItem = document.querySelector('.pictures');
 const usersGalleryItemTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const usersGalleryItemFragment = document.createDocumentFragment();
+const imageFilters = document.querySelector('.img-filters');
+const imageFiltersForm = document.querySelector('.img-filters__form');
+
+window.addEventListener('load', () => {
+  imageFilters.classList.remove('img-filters--inactive');
+});
 
 const createRecentUsersGalleryItem = (recentUsersGalleryItem) => {
+  document.querySelectorAll('.picture').forEach((element) => element.remove());
   recentUsersGalleryItem.forEach((element) => {
     const usersGalleryElement = usersGalleryItemTemplate.cloneNode(true);
     usersGalleryElement.querySelector('img').src = element.url;
@@ -18,4 +28,26 @@ const createRecentUsersGalleryItem = (recentUsersGalleryItem) => {
   usersGalleryItem.appendChild(usersGalleryItemFragment);
 };
 
-export {createRecentUsersGalleryItem};
+const setFilter = (data, cb) => {
+  imageFiltersForm.addEventListener('click', (evt) => {
+    imageFiltersForm.querySelectorAll('.img-filters__button').forEach((element) => element.classList.remove('img-filters__button--active'));
+    evt.target.classList.add('img-filters__button--active');
+    switch (evt.target.id) {
+      case 'filter-default' :
+        cb(data);
+        break;
+      case 'filter-random':
+        cb(getShuffleArray(data).slice(0, FILTER_RANDOM_MAX_ITEMS));
+        break;
+      case 'filter-discussed':
+        cb(data.slice().sort(getSortCommentsAsc));
+        break;
+    }
+  });
+};
+
+function getSortCommentsAsc(a, b) {
+  return b.comments.length - a.comments.length;
+}
+
+export {createRecentUsersGalleryItem, setFilter};
